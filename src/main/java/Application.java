26b80cb6419227c92ab2b1c53dc925e52adf58da
xutianyi1999 <853122726@qq.com>
2019-07-16 10:36:22 +0800

    优化

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,16 +3,12 @@ import com.beust.jcommander.Strings;
 import com.beust.jcommander.internal.Console;
 import common.Commons;
 import entity.ConfigEntity;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import receive.ReceiveHandler;
 import send.SendHandler;
 
 import java.io.File;
 
 public class Application {
-
-    private static final Logger LOGGER = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
         ConfigEntity configEntity = new ConfigEntity();
